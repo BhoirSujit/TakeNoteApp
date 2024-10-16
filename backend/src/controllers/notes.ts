7192e829d98a9537c3a41d883bf318213f068a1b
Sujit <bhoirsujit04>
2024-10-16 13:11:54 +0530

    added delete notes

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -104,3 +104,26 @@ export const updateNote: RequestHandler<
     next(error);
   }
 };
+
+interface DeleteNoteParam {
+  noteId: string;
+}
+
+export const deleteNote: RequestHandler<DeleteNoteParam, unknown, unknown, unknown> = async (req, res, next) => {
+  const {noteId} = req.params;
+  try {
+    if (!mongoose.isValidObjectId(noteId))
+      throw createHttpError(400, "Invalid note id");
+
+    const note = await NoteModel.findById(noteId).exec();
+
+    if (!note) throw createHttpError(404, "Note not found");
+
+    await note.deleteOne();
+
+    res.sendStatus(204);
+    
+  } catch (error) {
+    next(error);
+  }
+}
--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -1,13 +1,12 @@
 import * as NoteController from "../controllers/notes";
 import { Router } from "express";
 
-
 const router = Router();
 
-router.get("/", NoteController.getNotes );
+router.get("/", NoteController.getNotes);
 router.get("/:noteId", NoteController.getNote);
 router.post("/", NoteController.createNote);
-router.patch("/:noteId", NoteController.updateNote)
-
+router.patch("/:noteId", NoteController.updateNote);
+router.delete("/:noteId", NoteController.deleteNote);
 
 export default router;
