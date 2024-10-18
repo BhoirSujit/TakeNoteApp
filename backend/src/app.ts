bd3e13cc2e41555548142ad3b1a25e4c55df78f1
Sujit <bhoirsujit04>
2024-10-18 14:46:02 +0530

    fustration l5

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,34 +9,49 @@ import session from "express-session";
 import env from "./util/validate";
 import MongoStore from "connect-mongo";
 import { requiresAuth } from "./middleware/auth";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-  origin: ['http://localhost:5173','https://takenoteapp.onrender.com'],
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://takenoteapp.onrender.com"],
     credentials: true,
-  }));
+  })
+);
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://takenoteapp.onrender.com"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
-
-
+// Use cookies
+app.use(cookieParser());
 
 app.use(morgan("dev"));
 
 app.use(express.json());
 
-app.use(session({
-  secret: env.SESSION_SECREAT,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 60 * 60 * 1000,
-  },
-  rolling: true,
-  store: MongoStore.create({
-    mongoUrl: env.MONGO_CONNECTING_STRING,
+app.use(
+  session({
+    secret: env.SESSION_SECREAT,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+    },
+    rolling: true,
+    store: MongoStore.create({
+      mongoUrl: env.MONGO_CONNECTING_STRING,
+    }),
   })
-}));
+);
 
 app.use("/api/users", userRouter);
 app.use("/api/notes", requiresAuth, notesRouter);
