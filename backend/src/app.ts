311c7b004bc1cd21f04f28616f3329f5ff738862
Sujit <bhoirsujit04>
2024-10-18 14:30:02 +0530

    fustration l3

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,20 +12,11 @@ import { requiresAuth } from "./middleware/auth";
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:5173', 'https://takenoteapp.onrender.com'];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true, // Ensure credentials are sent with cross-origin requests
-}));
+  origin: ['http://localhost:5173','https://takenoteapp.onrender.com'],
+    credentials: true,
+  }));
 
-app.options('*', cors()); // Handle preflight requests
 
 
 
