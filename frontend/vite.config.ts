9d5e38f7277c7b2109a0179a7e1f78641e95d557
sujit bhoir <sujitbhoir004@gmail.com>
2024-11-23 00:37:01 +0530

    lets try 9

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build', // This ensures the build output is in a folder named 'build'
-  },
-  // Add this line to ensure SPA routing works on Render or other hosts:
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  // Enable fallback for react-router-dom (for SPA routing)
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  }
 })
