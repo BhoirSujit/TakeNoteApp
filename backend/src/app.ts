4a73d4d90bf33728b247e7d4ebccce1609efd70d
sujit bhoir <sujitbhoir004@gmail.com>
2024-11-23 00:02:40 +0530

    lets try 3

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -54,12 +54,12 @@ app.use("/api/notes", requiresAuth, notesRouter);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../../frontend/dist")));
   console.log("front end path : ",path.join(__dirname, "../../frontend/dist"));
 
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../../frontend", "dist", "index.html"));
   });  
 }
 
